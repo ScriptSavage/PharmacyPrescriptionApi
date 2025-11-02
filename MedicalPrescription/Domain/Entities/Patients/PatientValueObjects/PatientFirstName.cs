8002d461d87fc23e57b59282364fe47e0a22ad7b
maksymilian <maksymiliangaleziowski@gmail.com>
2025-11-02 14:54:35 +0100

    add PatientFirstName value object

---
+++ MedicalPrescription/Domain/Entities/Patients/PatientValueObjects/PatientFirstName.cs
@@ -0,0 +1,33 @@
+namespace Domain.Entities.PatientValueObjects;
+
+public class PatientFirstName
+{
+    private string _firstName;
+
+    public string FirstName
+    {
+        get => _firstName;
+        set
+        {
+            if (string.IsNullOrWhiteSpace(value) || value.Length < 1 || string.IsNullOrEmpty(value))
+            {
+                throw new ArgumentException("Invalid first name");
+            }
+            _firstName = value;
+        }
+    }
+
+    public PatientFirstName(string firstName)
+    {
+        FirstName = firstName;
+    }
+    
+    public static implicit operator string(PatientFirstName patientFirstName) => patientFirstName.FirstName;
+    public static implicit operator PatientFirstName(string firstName) => new PatientFirstName(firstName);
+
+    public override string ToString()
+    {
+        return $"Patient first name: {FirstName}";
+    }
+}
+    
