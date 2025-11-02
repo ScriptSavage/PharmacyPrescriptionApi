946608abe1d9995920fec8ce8d1caa7feaf0b3de
maksymilian <maksymiliangaleziowski@gmail.com>
2025-11-02 14:56:43 +0100

    add Patient Entity

---
+++ MedicalPrescription/Domain/Entities/Patients/Patient.cs
@@ -0,0 +1,44 @@
+using System.ComponentModel.DataAnnotations;
+using Domain.Entities.PatientValueObjects;
+
+namespace Domain.Entities.Patients;
+
+public class Patient
+{
+    public PatientId PatientId { get; private set; }
+    public PatientFirstName FirstName { get; private set; }
+    public PatientLastName LastName { get; private set; }
+    public PatientPesel Pesel { get; private set; }
+    public PatientPhoneNumber PhoneNumber { get; private set; }
+    public PatientEmailAddress EmailAddress { get; private set; }
+    
+    public Patient(PatientId patientId, PatientFirstName firstName, PatientLastName lastName, PatientPesel pesel, 
+        PatientPhoneNumber phoneNumber, PatientEmailAddress emailAddress)
+    {
+        PatientId = patientId;
+        FirstName = firstName;
+        LastName = lastName;
+        Pesel = pesel;
+        PhoneNumber = phoneNumber;
+        EmailAddress = emailAddress;
+    }
+
+    public Patient()
+    {
+    }
+
+    public void ChangeEmailAddress(string newEmailAddress)
+    {
+        if (string.IsNullOrWhiteSpace(newEmailAddress) || newEmailAddress.Length < 3 ||
+            string.IsNullOrWhiteSpace(newEmailAddress))
+        {
+            throw new ArgumentException("Email address not correct", nameof(newEmailAddress));
+        }
+
+        EmailAddress = newEmailAddress;
+    }
+
+
+}
+
+
